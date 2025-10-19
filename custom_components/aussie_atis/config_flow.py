71b5d61a16c5d1eb8bc64f42aad88367e9cc6183
BigByte76 <60337336+BigByte76@users.noreply.github.com>
2025-10-19 21:03:02 +1100

    Update config_flow.py

--- custom_components/aussie_atis/config_flow.py
+++ custom_components/aussie_atis/config_flow.py
@@ -1,29 +1,20 @@
 from homeassistant import config_entries
-from homeassistant.core import callback
 from homeassistant.helpers.selector import SelectSelector
-from homeassistant.helpers import config_validation as cv
 import voluptuous as vol
 
 class AussieAtisConfigFlow(config_entries.ConfigFlow, domain="aussie_atis"):
-    """Handle a config flow for Aussie ATIS."""
-
     VERSION = 1
 
-    def __init__(self):
-        """Initialize the flow."""
-        self.airports = []
-
     async def async_step_user(self, user_input=None):
-        """Handle the initial step."""
         if user_input is not None:
-            self.airports = user_input["airports"]
-            return self.async_create_entry(title="Australian ATIS", data={"airports": self.airports})
+            airports = user_input["airports"]
+            return self.async_create_entry(title="Australian ATIS", data={"airports": airports})
 
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
                 vol.Required("airports"): SelectSelector(
-                    options=["YMML", "YSSY", "YBBN", "YPPH", "YSCB"],
+                    options=["YSSY", "YMML", "YBBN", "YPPH", "YSCB"],
                     multiple=True
                 ),
             }),
