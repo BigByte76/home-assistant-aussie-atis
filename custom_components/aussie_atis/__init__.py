df1ff89058b876eaa5015699c228d5a965d7da1d
BigByte76 <60337336+BigByte76@users.noreply.github.com>
2025-10-19 19:26:36 +1100

    Delete custom_components/aussie_atis directory

--- custom_components/aussie_atis/__init__.py
+++
@@ -1,6 +0,0 @@
-async def async_setup_entry(hass, entry):
-    from .sensor import AussieAtisSensor
-    for code in entry.data["airports"]:
-        sensor = AussieAtisSensor(hass, code)
-        hass.async_create_task(sensor.async_add())
-    return True
