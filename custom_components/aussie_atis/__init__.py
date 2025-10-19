9a53c62ac3352746d4111d879143579792b543f5
BigByte76 <60337336+BigByte76@users.noreply.github.com>
2025-10-19 21:10:50 +1100

    Update __init__.py

--- custom_components/aussie_atis/__init__.py
+++ custom_components/aussie_atis/__init__.py
@@ -3,6 +3,5 @@ from homeassistant.helpers.typing import HomeAssistantType
 
 async def async_setup_entry(hass: HomeAssistantType, entry: ConfigEntry):
     """Set up the integration from a config entry."""
-    # Forward the setup to the sensor platform
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
