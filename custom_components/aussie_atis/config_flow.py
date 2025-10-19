cb765442a58fd36e0c053f223d6eed91637ca071
BigByte76 <60337336+BigByte76@users.noreply.github.com>
2025-10-19 20:16:45 +1100

    Update config_flow.py

--- custom_components/aussie_atis/config_flow.py
+++ custom_components/aussie_atis/config_flow.py
@@ -1,32 +1,40 @@
-# import logging
 import voluptuous as vol
 from homeassistant import config_entries
-import homeassistant.helpers.config_validation as cv
-from .const import DOMAIN, AIRPORTS
+from homeassistant.core import callback
+from .const import DOMAIN
 
-_LOGGER = logging.getLogger(__name__)
+AIRPORTS = [
+    "YSSY", "YMML", "YBBN", "YPPH", "YPAD", "YBCG", "YBCS", "YSCB",
+    "YMHB", "YPDN", "YBTL", "YMLT", "YWLM", "YBMK", "YBSU", "YPKA",
+    "YBRK", "YBAS", "YPPD", "YAMB", "YPED", "YPTN", "YMES", "YPEA",
+    "YSRI", "YBTL", "YPDN", "YBSG", "YPLM", "YCIN", "YGIG", "YGNB",
+    "YMPC", "YPWR", "YBOK", "YSNW"
+]
 
-@config_entries.HANDLERS.register(DOMAIN)
 class AussieAtisConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Aussie ATIS."""
 
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
+        """Handle the initial step."""
         errors = {}
         if user_input is not None:
             return self.async_create_entry(
-                title="Aussie ATIS",
-                data={"airports": user_input["airports"]}
+                title="Australian ATIS",
+                data={
+                    "airports": user_input["airports"]
+                }
             )
 
-        schema = vol.Schema({
-            vol.Required("airports", default=[]): cv.multi_select(AIRPORTS)
+        data_schema = vol.Schema({
+            vol.Required("airports", default=["YMML"]): vol.All(
+                vol.Length(min=1), vol.Unique(), vol.In(AIRPORTS)
+            )
         })
 
         return self.async_show_form(
             step_id="user",
-            data_schema=schema,
+            data_schema=data_schema,
             errors=errors
         )
-config_flow.py placeholder for multiple airport selection
